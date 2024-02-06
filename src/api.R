6a99e98cd48f859f2677aec63e9ead01fa594653
Chandra Y <cyemparala@tacc.utexas.edu>
2024-02-06 17:00:26 -0600

    more debug info

--- src/api.R
+++ src/api.R
@@ -11,6 +11,7 @@ get_api_data <- function(api_address, session) {
   print(api_address)
   print(session$request$HTTP_COOKIE)
   cookies <- convert_cookie_string(session$request$HTTP_COOKIE)
+  print(cookies)
   datastore_response <- GET(api_address, set_cookies(cookies))
   warn_for_status(datastore_response)
   json_text <- content(datastore_response, as = "text")

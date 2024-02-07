fc97a452fb4818b7fd50de28786a8c8d1ab4d64d
Chandra Y <cyemparala@tacc.utexas.edu>
2024-02-06 18:13:56 -0600

    remove debug logging

--- src/api.R
+++ src/api.R
@@ -11,9 +11,7 @@ convert_cookie_string <- function(cookie) {
 
 get_api_data <- function(api_address, session) {
   print(api_address)
-  print(session$request$HTTP_COOKIE)
   cookies <- convert_cookie_string(session$request$HTTP_COOKIE)
-  print(cookies)
   datastore_response <- GET(api_address, set_cookies(cookies))
   warn_for_status(datastore_response)
   json_text <- content(datastore_response, as = "text")
